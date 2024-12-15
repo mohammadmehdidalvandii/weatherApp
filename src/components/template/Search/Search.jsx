a62dfd94828cbd6b0cec7181fe5db5925401f3d7
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-12-15 16:36:28 +0330

    get search and get api weather & forecast

--- src/components/template/Search/Search.jsx
+++ src/components/template/Search/Search.jsx
@@ -1,12 +1,36 @@
 import React, { useState } from 'react';
 import './Search.css';
 import Loading from '../../module/Loading/Loading';
+import swal from 'sweetalert';
 
 function Search() {
   const [isLoading , setIsLoading] = useState(false);
+  const [city , setCity] = useState("");
+  const key_api = "e71388857ca1f6798e51fed62c6c3a39";
 
-  const handlerSearchCity = ()=>{
+  const handlerSearchCity = async ()=>{
     setIsLoading(true);
+    const res = await  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key_api}&units=metric`)
+    if(res.status === 200){
+      const weatherData = await res.json();
+      const {lat , lon} =weatherData
+      localStorage.setItem("weatherData", JSON.stringify(weatherData))
+
+      const forecast = await fetch( `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key_api}&units=metric`)
+      if(forecast.status === 200 ){
+        const forecastData = await res.json();
+        localStorage.setItem("forecastData" , JSON.stringify(forecastData))
+        setIsLoading(false);
+      }
+      }else{
+        setIsLoading(false)
+        swal({
+          title:"This city does not exist",
+          icon:"error",
+          buttons:"try",
+        })
+    }
+    // console.log(`http://openweathermap.org/img/wn/${res.data.weather[0].icon}.png`)
   }
 
   return (
@@ -17,7 +41,10 @@ function Search() {
                 onClick={handlerSearchCity}
                 >Search</button>
                 <button className="search_btn">Get Location</button>
-                <input type="text" className="search_input" />
+                <input type="text" className="search_input"
+                value={city}
+                onChange={e=>setCity(e.target.value)}
+                />
             </div>
         </div>
         {isLoading && (
