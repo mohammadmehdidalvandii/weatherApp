e8c6fd767721ec376412ef4254821bf772e046bb
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-12-15 14:54:06 +0330

    created section header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useRoutes } from 'react-router-dom';
 import routes from './routes'
+import Header from './components/module/Header/Header';
 
 function App() {
   const router = useRoutes(routes)
   return (
     <>
+    <Header/>
     {router}
     </>
   )
---
+++ src/components/module/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+
+function Header() {
+  return (
+    <section className="header">
+        <div className="container">
+            <h1 className="header_title">WEATHER <span>APP</span></h1>
+        </div>
+    </section>
+  )
+}
+
+export default Header
