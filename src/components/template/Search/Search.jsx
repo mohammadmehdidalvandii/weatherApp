c6961ad345f003fc247866d6e42fddb04dd9c371
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-12-17 14:43:51 +0330

    add logic clear data

--- src/components/template/Search/Search.jsx
+++ src/components/template/Search/Search.jsx
@@ -35,6 +35,11 @@ function Search() {
         localStorage.setItem("forecastData" , JSON.stringify(forecastData))
         setIsLoading(false);
       }
+      // clear Data LocalStorage 10min
+      setTimeout(() => {
+        localStorage.removeItem("weatherData");
+        localStorage.removeItem("forecastData");
+      }, 1000*60*10);
       setCity("")
       window.location.reload()
       }else{
